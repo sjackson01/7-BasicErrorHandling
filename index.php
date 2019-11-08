5d754f38b859762b59b05eb90e3c39f7d67498dc
Steven Jackson <stevenpjackson0@gmail.com>
2019-11-08 15:16:04 -0600

    Add:Error code for test

--- index.php
+++ index.php
@@ -49,8 +49,8 @@ echo error_function();
 echo "End of File.";
 */
 
-
-// Report simple running errors
+/*
+// Report All Errors 
 error_reporting(E_ALL);
 // Make sure they're on screen
 ini_set('display_errors', 1);
@@ -77,6 +77,18 @@ Foo::bar();
 Foo::nope();
 
 echo "We'll never get here.";
+*/
+
+// Makes sure we are logging errors
+ini_set("log_errors", 1);
+// Tell php where to log the errors
+ini_set("error_log", "php-error.txt");
+
+$error_levels = array("E_ALL","E_NOTICE","E_STRICT","E_DEPRECATED","E_ERROR","E_WARNING","E_PARSE");
+
+foreach ($errors as $error){
+  echo $error."<br />"
+}
 
 ?>
 
